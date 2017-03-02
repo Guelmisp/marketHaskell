1f38c9eb3200f4b97996063e13cae7be9e96e7dd
Miguel Pessoa <guelmisp@gmail.com>
2017-03-02 09:19:11 -0300

    added database

--- superHaskell.hs
+++ superHaskell.hs
@@ -1,6 +1,21 @@
 -- Passo 1
-type codigo = Int
-type nome   = String
-type preco  = Int
+type Codigo = Int
+type Nome   = String
+type Preco  = Int
 
-type Produtos = [(codigo, nome, preco)]
+type Produtos = [(Codigo, Nome, Preco)]
+
+-- Passo 2
+tabelaProdutos :: Produtos
+tabelaProdutos = [ (001, "Chocolate", 121)
+				 , (002, "Biscoito", 1010)
+				 , (003, "Laranja", 56)
+				 , (004, "Sabao", 21)
+				 , (005, "Batata Chips", 133)
+				 , (006, "Doritos", 450)]
+
+-- Passo 3
+type Carrinho = [Codigo]
+type Conta = [(Nome)]
+
+-- Passo 4
